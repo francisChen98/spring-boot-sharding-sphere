9f171ef2fdac50650f92b990f7214c61b55c3880
chenyh <francis_hua@163.com>
2020-11-04 23:23:11 +0800

    1

--- sub-db-table/src/main/java/com/oujiong/Application.java
+++ sub-db-table/src/main/java/com/oujiong/Application.java
@@ -11,7 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author xub
  * @date 2019/10/08 下午6:33
  */
-@SpringBootApplication(exclude = SpringBootConfiguration.class)
+//@SpringBootApplication(exclude = SpringBootConfiguration.class) 和config包对应
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
--- sub-db-table/src/main/java/com/oujiong/config/DataSourceUtil.java
+++ sub-db-table/src/main/java/com/oujiong/config/DataSourceUtil.java
@@ -1,31 +1,31 @@
-package com.oujiong.config;
-
-import com.alibaba.druid.pool.DruidDataSource;
-
-import javax.sql.DataSource;
-
-/**
- * @Description: 数据源配置
- *
- * @author xub
- * @date 2019/10/16 下午7:47
- */
-public final class DataSourceUtil {
-
-    private static final String HOST = "localhost";
-
-    private static final int PORT = 3306;
-
-    private static final String USER_NAME = "root";
-
-    private static final String PASSWORD = "123456";
-
-    public static DataSource createDataSource(final String dataSourceName) {
-        DruidDataSource result = new DruidDataSource();
-        result.setDriverClassName("com.mysql.jdbc.Driver");
-        result.setUrl(String.format("jdbc:mysql://%s:%s/%s?serverTimezone=UTC&characterEncoding=utf-8&useSSL=false&useUnicode=true&allowMultiQueries=true&autoReconnect=true", HOST, PORT, dataSourceName));
-        result.setUsername(USER_NAME);
-        result.setPassword(PASSWORD);
-        return result;
-    }
-}
+//package com.oujiong.config;
+//
+//import com.alibaba.druid.pool.DruidDataSource;
+//
+//import javax.sql.DataSource;
+//
+///**
+// * @Description: 数据源配置
+// *
+// * @author xub
+// * @date 2019/10/16 下午7:47
+// */
+//public final class DataSourceUtil {
+//
+//    private static final String HOST = "localhost";
+//
+//    private static final int PORT = 3306;
+//
+//    private static final String USER_NAME = "root";
+//
+//    private static final String PASSWORD = "123456";
+//
+//    public static DataSource createDataSource(final String dataSourceName) {
+//        DruidDataSource result = new DruidDataSource();
+//        result.setDriverClassName("com.mysql.jdbc.Driver");
+//        result.setUrl(String.format("jdbc:mysql://%s:%s/%s?serverTimezone=UTC&characterEncoding=utf-8&useSSL=false&useUnicode=true&allowMultiQueries=true&autoReconnect=true", HOST, PORT, dataSourceName));
+//        result.setUsername(USER_NAME);
+//        result.setPassword(PASSWORD);
+//        return result;
+//    }
+//}
